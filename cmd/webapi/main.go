c71eca2962e96a68ffcbceaab6a1cc8fc4b7342f
mlatsa <your_email_on_github@example.com>
2025-09-04 19:13:45 +0200

    Wire Vue to Go API; enable CORS; messaging endpoints working end-to-end

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -4,14 +4,23 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/mlatsa/WASAProject/internal/service/api"
 )
 
 func main() {
 	rt := api.NewRouter()
+
+	// Allow the Vite dev server to call the API
+	cors := handlers.CORS(
+		handlers.AllowedOrigins([]string{"http://localhost:5173"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
 	addr := ":3000"
 	log.Printf("listening on %s\n", addr)
-	if err := http.ListenAndServe(addr, rt.Handler()); err != nil {
+	if err := http.ListenAndServe(addr, cors(rt.Handler())); err != nil {
 		log.Fatal(err)
 	}
 }
