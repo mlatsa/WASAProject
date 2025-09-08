195fe1adb86302a0d5415fad94a3c8bb2c5165e9
mlatsa <your_email_on_github@example.com>
2025-09-08 18:17:30 +0200

    fix: add root main.go wrapper and expose webapi.Main()

--- cmd/webapi/main.go
+++ cmd/webapi/main.go
@@ -1,29 +1,17 @@
-package main
+package webapi
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 
-	"github.com/gorilla/handlers"
-	"github.com/mlatsa/WASAProject/service/api"
+    "github.com/mlatsa/WASAProject/service/api"
 )
 
-func main() {
-	rt := api.NewRouter()
-
-	cors := handlers.CORS(
-		handlers.AllowedOrigins([]string{
-			"http://localhost:5173", // Vite dev
-			"http://localhost:8080", // Nginx default
-			"http://localhost:8081", // Alt Nginx port you used
-		}),
-		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
-		handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
-	)
-
-	addr := ":3000"
-	log.Printf("listening on %s\n", addr)
-	if err := http.ListenAndServe(addr, cors(rt.Handler())); err != nil {
-		log.Fatal(err)
-	}
+func Main() {
+    rt := api.NewRouter()
+    addr := ":3000"
+    log.Printf("listening on %s\n", addr)
+    if err := http.ListenAndServe(addr, rt.Handler()); err != nil {
+        log.Fatal(err)
+    }
 }
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mlatsa/WASAProject/cmd/webapi"
+
+func main() {
+    webapi.Main()
+}
