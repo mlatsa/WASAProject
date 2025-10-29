242645f70934a403bc98629638799c18f2c48e27
Mariam Latsabidze <65814453+mariamlats@users.noreply.github.com>
2025-10-29 14:02:02 +0100

    Frontend minimal App.vue + main.js working; API ping wired; vendor updated

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -1,1 +1,17 @@
-<template><main style="padding:16px"><h1>WebUI is running</h1></main></template>
+<template>
+  <main style="padding:16px">
+    <h1>WebUI is running</h1>
+    <button @click="ping">Ping /v1/health</button>
+    <pre>{{ out }}</pre>
+  </main>
+</template>
+
+<script setup>
+import axios from '../services/axios'
+import { ref } from 'vue'
+const out = ref('')
+async function ping() {
+  const r = await axios.get('/v1/health')
+  out.value = JSON.stringify(r.data, null, 2)
+}
+</script>
